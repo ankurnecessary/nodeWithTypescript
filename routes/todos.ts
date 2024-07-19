bd5252657b200d5cf535fc761867f7a3b935350f
Ankur Gupta <5038755+ankurnecessary@users.noreply.github.com>
2024-07-20 07:43:45 +0930

    [post-todo]: Saving a new todo.

--- routes/todos.ts
+++ routes/todos.ts
@@ -5,9 +5,20 @@ const todos: Todo[] = [];
 
 const router = Router();
 
+// To fetch all the todos
 router.get('/', (req, res, next) => {
   res.status(200).json(todos);
 });
 
+// To save a todo
+router.post('/todo', (req, res, next) => {
+  const newTodo: Todo = {
+    id: new Date().toISOString(),
+    text: req.body.text
+  };
+
+  todos.push(newTodo);
+  res.status(201).json({ message: 'Todo saved sucessfully', todo: newTodo, todos });
+});
 
 export default router;
