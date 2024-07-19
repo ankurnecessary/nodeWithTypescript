3243571a6955027cfb42e15b68610fa89b9782a6
Ankur Gupta <5038755+ankurnecessary@users.noreply.github.com>
2024-07-20 06:52:50 +0930

    [body-parser-middleware]: Added body-parser middleware to parse the body of the API request. POST requests.

--- app.ts
+++ app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import todosRouter from './routes/todos';
+import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(todosRouter);
 
