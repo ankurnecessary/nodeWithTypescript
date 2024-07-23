5dc66d6c6117d1faefef9b046437935fff808ae9
ankurnecessary <5038755+ankurnecessary@users.noreply.github.com>
2024-07-23 11:19:02 +0930

    [CORS]: Enables API requests from almost any domain.

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,16 @@ const app = express();
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json());
 
+// To  handle CORS error in the browser
+app.use((req: Request, res: Response, next: NextFunction): void => {
+  // We can set these headers conditionally, if we want our APIs to be accessed from multiple domains.
+  // But right now we are entertaining requests from any domain
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(todosRouter);
 
 app.listen(5000, () => {
