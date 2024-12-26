777031621cbaa0d71a7141f35ce3920c4dd2b16e
Ankur <5038755+ankurnecessary@users.noreply.github.com>
2024-12-27 05:15:10 +1030

    [code-formatting]: Configured code formatting in VS Code
    Added some packages and configurations to get the code formatted uniformly.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,19 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: 'standard-with-typescript',
+  extends: [
+    'standard-with-typescript',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'prettier'
+  ],
+  plugins: ['prettier'],
   overrides: [
     {
       env: {
         node: true
       },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
         sourceType: 'script'
       }
@@ -25,16 +29,25 @@ module.exports = {
   rules: {
     semi: ['error', 'always'], // Enforces semicolons at the end of statements
     '@typescript-eslint/semi': ['error', 'always'], // Ensures compatibility with TypeScript rules
-    '@typescript-eslint/member-delimiter-style': ['error', {
-      multiline: {
-        delimiter: 'semi', // Enforce semicolons for multiline
-        requireLast: true
-      },
-      singleline: {
-        delimiter: 'semi', // Enforce semicolons for singleline
-        requireLast: true
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'semi', // Enforce semicolons for multiline
+          requireLast: true
+        },
+        singleline: {
+          delimiter: 'semi', // Enforce semicolons for singleline
+          requireLast: true
+        }
       }
-    }]
+    ],
+    'prettier/prettier': 'error', // Enforces Prettier rules
+    'no-multi-spaces': ['error', { ignoreEOLComments: false }],
+    'no-trailing-spaces': 'error',
+    'space-in-parens': ['error', 'never'],
+    'template-curly-spacing': ['error', 'never'],
+    'block-spacing': ['error', 'always']
   },
   ignorePatterns: ['dist/**'] // Add this line to ignore the dist directory
 };
